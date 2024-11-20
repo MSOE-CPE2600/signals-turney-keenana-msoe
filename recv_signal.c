8de7c72eab2a8416c71733b97af79d7e2f953a00
Keenana <keenana@msoe.edu>
2024-11-20 11:25:10 -0600

    finished send_sig

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,40 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Andrew Keenan
  * 
- * Brief summary of program:
+ * Brief summary of program: recieves a signal and prints out the int
  */
+
+#include <signal.h>
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+
+ void handler(int sig, siginfo_t *info, void *context) {
+    if (info != NULL) {
+        printf("Received integer: %d\n", info->si_value.sival_int);
+    }
+    exit(1);
+ }
+
+ int main() {
+    // print pid for later
+    int pid = getpid();
+    printf("%d\n", pid);
+
+    // initialize struct
+    struct sigaction sa;
+
+    // setup flags and handlers
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+
+    // pass through struct using sigaction
+    sigaction(SIGUSR1, &sa, NULL);
+    
+    while (1) {
+        sleep(1);
+        // send the SIGUSR1 signal using kill in console
+    }
+    return 0;
+ }
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,30 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Andrew Keenan
  * 
- * Brief summary of program:
+ * Brief summary of program: sends a signal to a process
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <signal.h>
+
+int main (int argc, char *argv[]) {
+    int pid;
+    if (argc == 2) {
+        pid = atoi(argv[1]);
+    } else {
+        printf("Error with command line\n");
+        exit(1);
+    }
+
+    // setup random number
+    srand(time(NULL));
+    int random = rand() % 100;
+
+    union sigval signal;
+    signal.sival_int = random;
+
+    sigqueue(pid, SIGUSR1, signal);
+    return 0;
+}
