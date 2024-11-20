f9338c60e2c599a8d279767e8d07328c08e75e48
Keenana <keenana@msoe.edu>
2024-11-20 10:59:49 -0600

    finished sigaction

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,47 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_sigaction.c
+ * Modified by: Andrew Keenan
  * 
- * Brief summary of program:
+ * Brief summary of program: Uses sigaction to handle SIGUSR1
  */
+
+ #include <signal.h>
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+
+ void handler(int sig, siginfo_t *info, void *context) {
+    int pid;
+    if (info != NULL) {
+        pid = info->si_pid;
+    } else {
+        printf("error getting info\n");
+        exit(1);
+    }
+    printf("pid of user that sent SIGUSR1: %d\n", pid);
+    exit(1);
+ }
+
+ int main() {
+    // print pid for later
+    int pid = getpid();
+    printf("%d\n", pid);
+
+    // initialize struct
+    struct sigaction sa;
+
+    // setup flags and handlers
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+
+    // pass through struct using sigaction
+    sigaction(SIGUSR1, &sa, NULL);
+    
+    while (1) {
+        sleep(1);
+        // send the SIGUSR1 signal using kill in console
+    }
+    return 0;
+ }
 
 
