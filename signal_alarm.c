28654c09dbb114eea8cc5781367104c1daaca037
Keenana <keenana@msoe.edu>
2024-11-20 10:19:38 -0600

    Finished sigalarm

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,27 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_alarm.c
+ * Modified by: Andrew Keenan
  * 
- * Brief summary of program:
+ * Brief summary of program: Sets off an alarm after 5 seconds
  */
+
+ #include <signal.h>
+ #include <unistd.h>
+ #include <stdio.h>
+
+ void signal_handler() {
+    printf("ALARM EXPIRED\n");
+ }
+
+ int main() {
+    // register signal handler
+    signal(SIGALRM, signal_handler);
+
+    // set the alarm
+    alarm(5);
+
+    // sleep for 8
+    sleep(8);
+
+    return 0;
+ }
